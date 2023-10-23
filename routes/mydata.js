b535aef741d8c7ede2c370100dc1b296b053b0ed
kaushikvenna <vennakaushikramkoteshwar@gmail.com>
2023-10-23 17:55:34 -0500

    edited index

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-sum = 0;
-more = 0;
-/* GET users listing. */
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  more = more+1;
-  sum+=more;
-  res.send(`sum is ${sum}`);
+  res.render('mydata', { title: 'Venna kaushik Ram Koteshwar' });
 });
 
 module.exports = router;
